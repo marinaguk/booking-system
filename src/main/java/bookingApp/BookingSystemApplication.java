911be827f8bbc000b8d15f120a144b510823729e
Marina Guk <marin.naguk@gmail.com>
2026-09-23 16:14:32 +0300

    Set up Spring Boot: pom, entry point, config

---
+++ src/main/java/bookingApp/BookingSystemApplication.java
@@ -0,0 +1,11 @@
+package bookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSystemApplication.class, args);
+    }
+}
